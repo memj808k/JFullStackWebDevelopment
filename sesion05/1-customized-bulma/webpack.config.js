f71efa8ac63f4ad2f953e9d73fc78eaf71f7633e
memj808k <jesicammeza@gmail.com>
2023-04-01 14:01:38 -0700

    nueva sesion

---
+++ sesion05/1-customized-bulma/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            // options...
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'css/mystyles.css' }),
+        new HtmlWebpackPlugin({ template: 'src/index.html' })
+    ]
+}
